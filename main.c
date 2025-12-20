42f92b31b62c627ff994e16c8c89150d32fea401
Faseeh Ur Rehman <faseehurrehman499@gmail.com>
2025-12-20 13:17:28 +0500

    Function Prototypes and Global Variables declared

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 /*
 * Name: main.c
 * Purpose: A complete chess implementation with all standard rules including
-*          en passant and castling, using Raylib for graphics rendering
+*          en passant and castling, using Raylib for graphics rendering.
+*          Inspire from Chess.com
 * Author: Faseeh-Ur-Rehman
 * Roll Number: FA25-BCS-019
 
@@ -19,3 +20,169 @@
 * - Algebraic notation panel at the side bar to replay the game
 */
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<math.h>
+
+#include "raylib.h"
+
+#define TILE_SIZE 80
+#define BOARD_SIZE 8
+#define BUFFER_SIZE 128
+
+#define TILE_LIGHT GetColor(0xEEEED2FF)
+#define TILE_DARK  GetColor(0x769656FF)
+
+/**
+ * PieceType enum: represents all possible chess pieces
+ */
+typedef enum{
+    EMPTY,
+    PAWN,
+    ROOK,
+    KNIGHT,
+    BISHOP,
+    QUEEN,
+    KING
+} PieceType;
+
+/**
+ * PieceColor enum: represents piece Colors. White moves First
+ */
+typedef enum{
+    WHITE_PIECE,
+    BLACK_PIECE,
+    NONE_PIECE
+} PieceColor;
+
+/**
+ * Piece struct: Complete representation of a chess piece
+ *
+ * bool moved: checks eligibility for castling and pawn double-move
+ * bool enPassant: flag for enPassant move
+ *
+ * en passant is special pawn capture in chess where a pawn
+ * can capture an opponent's pawn that has just moved two squares
+ * forward from its starting position, landing right beside it,
+ * as if the opponent's pawn had only moved one square
+ */
+typedef struct{
+    PieceType type;
+    PieceColor color;
+    bool moved;
+    bool enPassant;
+} Piece;
+
+Piece board[BOARD_SIZE][BOARD_SIZE];
+int selectedRow = -1;
+int selectedCol = -1;
+PieceColor turn = WHITE_PIECE;
+bool gameOver = false;
+
+char gameResult[BUFFER_SIZE] = {0};
+
+// en passant tracking
+int enPassantTargetRow = -1;
+int enPassantTargetCol = -1;
+PieceColor enPassantPawnColor = NONE_PIECE;
+
+// array to store Textures (I will be using pngs as piece models from the web)
+// Texture array[color][type];
+Texture2D pieceTextures[2][7];
+
+/*============= Core Game Functions =================*/
+void InitBoard(void);
+void LoadAssets(void);
+void UnloadAssets(void);
+void DrawBoard(void);
+void DrawPieces(void);
+void HandleInput(void);
+
+/*============ Move Validations ======================*/
+bool IsValidMove(int source_row, int source_column, int destination_row, int destination_column);
+bool MovePiece(int source_row, int source_column, int destination_row, int destination_column);
+bool IsInCheck(PieceColor color);
+bool HasAnyValidMove(PieceColor color);
+bool IsCheckMate(PieceColor Color);
+bool IsPathClear(int source_row, int source_column, int destination_row, int destination_column);
+bool TestMoveForCheck(PieceColor color, int source_row, int source_column, int destination_row, int destination_column);
+void ResetEnPassant(void);
+
+int main(void){
+
+    InitWindow(BOARD_SIZE * TILE_SIZE + 240,BOARD_SIZE * TILE_SIZE,"Chess- Faseeh-Ur-Rehman");
+    SetTargetFPS(60);
+
+
+
+    CloseWindow();
+    return EXIT_SUCCESS;
+}
+
+//===========================================================================
+// ASSET MANAGEMENT FUNCTIONS
+//===========================================================================
+
+void LoadAssets(void){
+
+}
+void UnloadAssets(void){
+
+}
+
+//===========================================================================
+// BOARD INITIALIZATION FUNCTION
+//===========================================================================
+
+void InitBoard(void){
+
+}
+
+//===========================================================================
+// RENDERING FUNCTIONS
+//===========================================================================
+
+void DrawBoard(void){
+
+}
+void DrawPieces(void){
+
+}
+
+//===========================================================================
+// INPUT HANDLING FUNCTION
+//===========================================================================
+
+void HandleInput(void){
+
+}
+
+//===========================================================================
+// MOVE VALIDATION FUNCTIONS
+//===========================================================================
+
+bool IsValidMove(int sr, int sc, int dr, int dc){
+
+}
+bool MovePiece(int sr, int sc, int dr, int dc){
+
+}
+bool IsInCheck(PieceColor color){
+
+}
+bool HasAnyValidMove(PieceColor color){
+
+}
+bool IsCheckMate(PieceColor Color){
+
+}
+bool IsPathClear(int sr, int sc, int dr, int dc){
+
+}
+bool TestMoveForCheck(PieceColor color, int sr, int sc, int dr, int dc){
+
+}
+void ResetEnPassant(void){
+
+}
